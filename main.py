74ac4b5db7e67c4090e3cecd943f964bfba0cc4c
henriquehfl <henriqueflimas@gmail.com>
2026-07-18 18:50:52 -0300

    update

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ YDL_OPTIONS = {
     'noplaylist': True,
     'quiet': True,
     'no_warnings': True,
+    'js_runtimes': ['node'],
 }
 
 YDL_PLAYLIST_OPTIONS = {
@@ -23,6 +24,7 @@ YDL_PLAYLIST_OPTIONS = {
     'quiet': True,
     'no_warnings': True,
     'extract_flat': True,
+    'js_runtimes': ['node'],
 }
 
 FFMPEG_OPTIONS = {
